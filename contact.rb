ac0ded68852f07192f933fe9540066b14e4e74b0
b-silva <berna.loyola@gmail.com>
2016-04-06 00:29:21 +0000

    refactored methods and applied better practices

--- contact.rb
+++ contact.rb
@@ -4,7 +4,7 @@ require 'csv'
 # The ContactList class will work with Contact objects instead of interacting with the CSV file directly
 class Contact
   
-  @@csv_file = './contacts.csv'
+  CSV_FILE = './contacts.csv'
 
   attr_accessor :name, :email, :id
   
@@ -12,7 +12,7 @@ class Contact
   # @param name [String] The contact's name
   # @param email [String] The contact's email address
   def initialize(name, email, id = nil)
-    # TODO: Assign parameter values to instance variables.
+    # Assign parameter values to instance variables.
     @name = name
     @email = email
     @id = id
@@ -24,9 +24,9 @@ class Contact
     # Opens 'contacts.csv' and creates a Contact object for each line in the file (aka each contact).
     # @return [Array<Contact>] Array of Contact objects
     def all
-      # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
+      # Return an Array of Contact instances made from the data in 'contacts.csv'.
       list = []
-      CSV.foreach(@@csv_file) do |contact|
+      CSV.foreach(CSV_FILE) do |contact|
         name = contact[0]
         email = contact[1]
         id = $.
@@ -39,35 +39,35 @@ class Contact
     # Creates a new contact, adding it to the csv file, returning the new contact.
     # @param name [String] the new contact's name
     # @param email [String] the contact's email
-    def create(name, email, id = nil, *phones)
-      # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
+    def create(name, email, phones = nil)
+      # Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
       return nil unless search(email).empty?
-      size = CSV.read(@@csv_file).size
-      csv = CSV.open(@@csv_file, 'a', converters: :line)
+      size = CSV.read(CSV_FILE).size
+      csv = CSV.open(CSV_FILE, 'a')
       if phones.empty?
         csv << [name,email]
       else
-        csv << [name,email,phones.join]
+        csv << [name,email,phones]
       end
       id = size + 1
 
-      contact = Contact.new(name, email, id)
+      Contact.new(name, email, id)
     end
     
     # Find the Contact in the 'contacts.csv' file with the matching id.
     # @param id [Integer] the contact id
     # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
-      # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
-      csv_array = CSV.read(@@csv_file)
-      csv_array[id - 1] ? csv_array[id - 1] : nil
+      # Find the Contact in the 'contacts.csv' file with the matching id.
+      csv_array = CSV.read(CSV_FILE)
+      csv_array[id - 1]
     end
     
     # Search for contacts by either name or email.
     # @param term [String] the name fragment or email fragment to search for
     # @return [Array<Contact>] Array of Contact objects.
     def search(term)
-      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
+      # Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
       Contact.all.select do |contact|
         contact.name =~ /#{term}/i || contact.email =~ /#{term}/i
       end
